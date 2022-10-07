e32d874ca60e47647dda9d7a3b1a9a24d8c23b20
David Venegas <venegasdavidm@gmail.com>
2022-10-07 13:44:19 -0400

    cleaner demo

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
-import { useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import io from 'socket.io-client'
 const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const inputRef = useRef()
+  const [messages, setMessages] = useState([])
+
   const sendMessage = () => {
-    socket.emit("send_message", {message: 'hello'})
+    socket.emit("send_message", { message: inputRef.current.value})
+    inputRef.current.value = ""
   }
 
   useEffect(() => {
-    socket.on("recive_message", (data) => {
-      alert(data.message)
-    })
+    const trigger = (data) => {
+      setMessages(messages => [...messages, data.message])
+    }
+    socket.on("recive_message", trigger)
+    return () => {
+      socket.off("recive_message", trigger);
+    }
   }, [socket])
-  
 
   return (
     <div className="app">
-      <input type="text" placeholder="Message..." />
-      <button onClick={sendMessage}>Send Message</button>
+      <div className="input-box">
+        <input type="text" placeholder="Message..." ref={inputRef} />
+        <button onClick={sendMessage}>Send Message</button>
+      </div>
+      <div className="message-box">
+        <h1>Messages:</h1>
+        <ul>
+          {messages.map(message => (
+            <li key={Math.random()}>{message}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
